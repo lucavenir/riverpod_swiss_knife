c0072891a35481b2af9ae22d185f340970f0ba27
~venir <luca@venir.dev>
2025-11-22 22:18:03 +0100

    better typedefs

--- lib/src/core/fn.dart
+++ lib/src/core/fn.dart
@@ -1,8 +1,15 @@
+import "dart:async";
+
 typedef VoidCallback = void Function();
 typedef ValueSetter<T> = void Function(T value);
 typedef ValueGetter<T> = T Function();
 
-typedef AsyncValueGetter<T> = Future<T> Function();
-typedef AsyncValueSetter<T> = Future<void> Function(T value);
+typedef FutureVoidCallback = Future<void> Function();
+typedef FutureValueGetter<T> = Future<T> Function();
+typedef FutureValueSetter<T> = Future<void> Function(T value);
+
+typedef FutureOrVoidCallback = FutureOr<void> Function();
+typedef FutureOrValueGetter<T> = FutureOr<T> Function();
+typedef FutureOrValueSetter<T> = FutureOr<void> Function(T value);
 
 typedef Fn<In, Out> = Out Function(In input);
